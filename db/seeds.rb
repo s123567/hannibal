8b99b363127a9fc46134e57c8f5c34b52315f1fe
Mathieu Chiron <mathieu_chiron@yahoo.com>
2016-11-10 15:58:28 +0100

    added seed with users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+bob = User.create!(email: 'bob@bob.com', password: 'testtest', password_confirmation: 'testtest')
+joe = User.create!(email: 'joe@joe.com', password: 'testtest', password_confirmation: 'testtest')
+
+Quote.create!(content: "quote1", user: bob)
+Quote.create!(content: "quote2", user: bob)
+Quote.create!(content: "quote3", user: bob)
+Quote.create!(content: "quote4", user: joe)
+Quote.create!(content: "quote5", user: joe)
+Quote.create!(content: "quote6", user: joe)
+Quote.create!(content: "quote7", user: joe)
