eb92ff655dd089e421c024a9a5183b6959eb7770
Mathieu Chiron <mathieu_chiron@yahoo.com>
2016-11-11 15:13:50 +0100

    added model, associations, validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,40 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-         has_many :quotes
-         has_many :upvotes
+  has_many :quotes
+  has_many :upvotes
 
-         def upvoted?(quote)
-          quote.upvotes.where(user: self).any?
-         end
+  has_many :active_relationships, class_name: "Relationship",
+                                 foreign_key: "follower_id",
+                                 dependent: :destroy
+
+  has_many :passive_relationships, class_name: "Relationship",
+                                 foreign_key: "followed_id",
+                                 dependent: :destroy
+
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :active_relationships, source: :follower
+
+
+   # Follows a user.
+     def follow(other_user)
+       active_relationships.create(followed_id: other_user.id)
+     end
+
+     # Unfollows a user.
+     def unfollow(other_user)
+       active_relationships.find_by(followed_id: other_user.id).destroy
+     end
+
+     # Returns true if the current user is following the other user.
+     def following?(other_user)
+       following.include?(other_user)
+     end
+
+
+
+
+  def upvoted?(quote)
+   quote.upvotes.where(user: self).any?
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161110150816) do
+ActiveRecord::Schema.define(version: 20161111140341) do
 
   create_table "quotes", force: :cascade do |t|
     t.text     "content"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 20161110150816) do
     t.datetime "updated_at", null: false
     t.integer  "user_id"
     t.index ["user_id"], name: "index_quotes_on_user_id"
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer  "follower_id"
+    t.integer  "followed_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "upvotes", force: :cascade do |t|
