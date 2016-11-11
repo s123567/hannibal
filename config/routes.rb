8bad4aa4a11a07eefbcef42fe2038f326251aaf5
Mathieu Chiron <mathieu_chiron@yahoo.com>
2016-11-11 16:04:35 +0100

    Homegae:link to each quote author profile page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#home'
   resources :upvotes, only: [:create, :destroy]
 
   resources :quotes, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
   devise_scope :user do 
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
