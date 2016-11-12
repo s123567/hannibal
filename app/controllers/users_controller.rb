38ffee68dd2b69c1699d826a3bb31cb12828e482
Mathieu Chiron <mathieu_chiron@yahoo.com>
2016-11-12 08:22:12 +0100

    implemented follower/following relationship

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,33 @@
 class UsersController < ApplicationController
+before_action :find_user, only:[:destroy]
   def show
     @user = User.find(params[:id])
     @quotes = @user.quotes
     
   end
+
+  def show_follow
+    
+  end
+
+  def following
+    @title = "Following"
+    @user = User.find(params[:id])
+    @users = @user.following
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @user = User.find(params[:id])
+    @users = @user.followers
+    render 'show_follow'
+  end
+private
+def find_user
+  @user = User.find(params[:id])
+  
+end
+
+
 end
