4675afd939eb42e6eb523436f91824eee19599aa
Mathieu Chiron <mathieu_chiron@yahoo.com>
2016-12-19 20:53:48 +0100

    added quote rotator

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,7 @@ class PagesController < ApplicationController
     @quote = Quote.new
     @quotes = Quote.search(params[:keyword])
     @user = current_user
+    @random_quote = @quotes.sample
     if user_signed_in?
       @user_quotes = @user.quotes
         else
