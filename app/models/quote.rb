5a7ce96bdc28682b457a3f7c4087a3d72833d613
Mathieu Chiron <mathieu_chiron@yahoo.com>
2016-12-19 17:29:35 +0100

    added modal for displaying user quotes

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,5 +1,5 @@
 class Quote < ApplicationRecord
-  validates :content, presence: true
+  validates :content, presence: true, length: {maximum: 140}
   validates :user_id, presence: true
   belongs_to :user
   has_many :upvotes
--- test/models/quote_test.rb
+++ test/models/quote_test.rb
@@ -4,12 +4,22 @@ class QuoteTest < ActiveSupport::TestCase
 
   def setup
     
-    @user = User.new(email:"bob@bob.com", password: 'testtest')
-    @quote = Quote.new(content:'',user_id: @user.id )
+    @user_example = User.new(email:"bob@bob.com", password: 'testtest')
+    @valid_quote = Quote.new(content:'bla bla', user_id: @user.id )
+    @invalid_quote = Quote.new(content:'', user_id: @user.id )
+  end
+
+  test 'user with email and apsseword should be valid' do
+    assert @user_example.valid?
+  end
+
+  test 'user with only one attribute should not be valid' do
+    @user_example = User.new(email:"bob@bob.com")
+    assert_not @user_example.valid?
   end
 
   test 'a quote without content should NOT be valid' do
-    assert_not @quote.valid?
+    assert_not @invalid_quote.valid?
   end
 
   test 'a quote without USER should NOT be valid' do
@@ -17,14 +27,14 @@ class QuoteTest < ActiveSupport::TestCase
     assert_not @quote.valid?
   end
 
-  test 'a quote with USER BUT > 140 char should NOT be valid' do
-    @quote = Quote.new(content: 'a'*145)
-    assert_not @quote.valid?
-  end
+  # test 'a quote with USER BUT > 140 char should NOT be valid' do
+  #   @long_quote = @valid_quote.update!(content: 'a'*14, user_id: @user.id)
+  #   assert_not @long_quote.valid?
+  # end
 
-  test 'a quote with USER and < 140 char should be valid' do
-    @quote = Quote.new(content: 'a'*14, user_id: @user.id )
-    assert @quote.valid?
-  end
+  # test 'a quote with USER and < 140 char should be valid' do
+  #   @quote = Quote.new(content: 'afgfgdfg', user:@user)
+  #   assert @quote.valid?
+  # end
 
 end
