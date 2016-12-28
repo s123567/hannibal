f0a7b9d2968e7f026aaaabf8b1d1e8935e3d497d
Mathieu Chiron <mathieu_chiron@yahoo.com>
2016-12-28 09:58:53 +0100

    put Quote and feed inside a panel w/tab

--- app/models/user.rb
+++ app/models/user.rb
@@ -41,7 +41,6 @@ class User < ApplicationRecord
 
      def feed
       following_ids = "SELECT followed_id FROM relationships WHERE follower_id = :user_id"
-
       Quote.where("user_id IN (#{following_ids}) OR user_id = :user_id", user_id: id)
        
      end
